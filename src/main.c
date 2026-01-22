a10e4b332686ae9457dcbd13c9624ef6aa154695
Aschermer <Github@aschermer.com>
2026-01-22 16:12:32 -0600

    Copy close to working

--- src/main.c
+++ src/main.c
@@ -49,34 +49,69 @@ void deleteDirectory(char *path)
     return;
 }
 
-void copyDirectory(char *originalPath, char *newPath)
+void copyDirectory(char *dstPath, char *srcPath)
 {
+    char originalPath[4096];
+    char newPath[4096];
+
+    strcpy(originalPath, dstPath);
+    strcpy(newPath, srcPath);
+
     printf("Copying %s to %s\n", originalPath, newPath);
-    if(chdir(newPath) != 0)
-    {
-        printf("Failed to Change Directories\n");
-    }
-    DIR *d;
+    
     struct dirent *dir;
-    d = opendir(".");
-    if (d) {
-        while ((dir = readdir(d)) != NULL) {
-            if(!(strcmp(dir->d_name, ".") == 0) && !(strcmp(dir->d_name, "..") == 0))
-            {
-                printf("Name: %10s  Type: %d\n", dir->d_name, dir->d_type);
-                if(dir->d_type == 4)
-                {
-                    
-                }
-                if(dir->d_type == 8)
-                {
-                    
-                }
-            }
-        }
-        closedir(d);
-        chdir("..");
+    
+    DIR *d;
+    d = opendir(originalPath);
+    if (!d) 
+    {
+        printf("Failed to Open File %s\n", originalPath);
     }
+
+    mkdir(newPath, 0777);
+
+    while ((dir = readdir(d)) != NULL)
+    {
+        if(!(strcmp(dir->d_name, ".") == 0) && !(strcmp(dir->d_name, "..") == 0))
+        {
+            printf("Name: %10s  Type: %d\n", dir->d_name, dir->d_type);
+
+            strcat(originalPath, "/");
+            strcat(newPath, "/");
+
+            strcat(originalPath, dir->d_name);
+            strcat(newPath, dir->d_name);
+
+            if(dir->d_type == 4)
+            {
+                copyDirectory(originalPath, newPath);
+            }
+            else if(dir->d_type == 8)
+            {
+                FILE *srcFile = fopen(originalPath, "r");
+                FILE *dstFile = fopen(newPath, "w");
+
+                char byteBuffer[1024];
+                int bytesRead;
+                while((bytesRead = fread(byteBuffer, 1, sizeof(byteBuffer), srcFile)))
+                {
+                    if(fwrite(byteBuffer, 1, bytesRead, dstFile) != bytesRead)
+                    {
+                        printf("Error Copying File %s to %s\n", originalPath, newPath);
+                        break;
+                    }
+                }
+
+                fclose(srcFile);
+                fclose(dstFile);
+            }
+
+            strcpy(originalPath, dstPath);
+            strcpy(newPath, srcPath);
+        }
+    }
+    closedir(d);
+    chdir("..");
     return;
 }
 
@@ -96,9 +131,9 @@ void parseCommands(char *command, uint32_t optionBits, char *option1, char *opti
     printf("Option1: %s\n", option1);
     printf("Option2: %s\n", option2);
 
-    if(strcmp(option1, "pman") == 0)
+    if((strcmp(option1, "pman") == 0) || (strcmp(option2, "pman") == 0))
     {
-        printf("Cannot Edit This Directory\n");
+        printf("Cannot Edit PMAN Directory\n");
         return;
     }
 
@@ -141,7 +176,7 @@ void parseCommands(char *command, uint32_t optionBits, char *option1, char *opti
     }
     else if(strcmp(command, "copy") == 0)
     {
-        
+        copyDirectory(option1, option2);
     }
     else 
     {
