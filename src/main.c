6f1867541411f6ab819ac310424ce206cbc685b9
Aschermer <Github@aschermer.com>
2026-01-21 22:07:24 -0600

    Basic Project Deletion

--- src/main.c
+++ src/main.c
@@ -5,11 +5,49 @@
 #include <string.h>
 #include <ctype.h>
 #include <sys/stat.h>
+#include <dirent.h>
 
 // 32 bit flags
 enum OPTIONFLAGS{
     NONE = 0x0000
 };
+
+void deleteDirectory(char *path)
+{
+    printf("Deleting %s\n", path);
+    char *newPath = (char *)malloc((strlen(path) + 3) * sizeof(char));
+    newPath[0] = '.';
+    newPath[1] = '/';
+    newPath[2] = '\0';
+    strcat(newPath, path);
+    if(chdir(newPath) != 0)
+    {
+        printf("Failed to Change Directories\n");
+    }
+    DIR *d;
+    struct dirent *dir;
+    d = opendir(".");
+    if (d) {
+        while ((dir = readdir(d)) != NULL) {
+            if(!(strcmp(dir->d_name, ".") == 0) && !(strcmp(dir->d_name, "..") == 0))
+            {
+                printf("Name: %10s  Type: %d\n", dir->d_name, dir->d_type);
+                if(dir->d_type == 4)
+                {
+                    deleteDirectory(dir->d_name);
+                }
+                if(dir->d_type == 8)
+                {
+                    remove(dir->d_name);
+                }
+            }
+        }
+        closedir(d);
+        chdir("..");
+        rmdir(path);
+    }
+    return;
+}
 
 int parseOptions(char *arg)
 {
@@ -56,10 +94,7 @@ void parseCommands(char *command, uint32_t optionBits, char *option1, char *opti
     }
     else if(strcmp(command, "create") == 0)
     {
-        char *path = (char *)malloc((strlen(option1) + 2) * sizeof(char));
-        path = "./";
-        strcat(path, option1);
-        mkdir(path, 0777);
+        mkdir(option1, 0777);
         if(strcmp(option2, "c") == 0)
         {
 
@@ -67,7 +102,7 @@ void parseCommands(char *command, uint32_t optionBits, char *option1, char *opti
     }
     else if(strcmp(command, "delete") == 0)
     {
-        
+        deleteDirectory(option1);
     }
     else if(strcmp(command, "rename") == 0)
     {
