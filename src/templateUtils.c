e8b3257370f0653bc3ed3424317c25ccbdfed0c6
Aschermer <Github@aschermer.com>
2026-01-31 22:57:37 -0600

    Added initial commit on git repo creation

--- src/templateUtils.c
+++ src/templateUtils.c
@@ -26,7 +26,37 @@ void defaultTemplate(char *name)
         printf("ERROR CREATING REPO\n");
     }
 
+    git_signature *sig;
+    git_index *index;
+    git_oid tree_id, commit_id;
+    git_tree *tree;
+
+    if(git_signature_default(&sig, repo) < 0)
+    {
+        printf("Error creating initial commit signature\n");
+    }
+
+    if(git_repository_index(&index, repo) < 0)
+    {
+        printf("Error opening repo index\n");
+    }
+
+    if(git_index_write_tree(&tree_id, index) < 0)
+    {
+        printf("Error writing initial tree\n");
+    }
+
+    if(git_tree_lookup(&tree, repo, &tree_id))
+    {
+        printf("Error looking up git tree\n");
+    }
+
+    if(git_commit_create_v(&commit_id, repo, "HEAD", sig, sig, NULL, "Initial Commit", tree, 0) < 0)
+    {
+        printf("Error creating commit\n");
+    }
+
+    git_index_free(index);
     git_repository_free(repo);
     git_libgit2_shutdown();
-
 }
